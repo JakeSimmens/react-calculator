bebb5f51f0c0012844af2dec954959d2af5232cb
Jake Simmens <rocknjake@gmail.com>
2020-12-26 21:47:36 -0600

    Functions for + - x / work properly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import './App.css';
 
 function App() {
@@ -8,8 +8,6 @@ function App() {
 
     <div className="container">
       <Calculator />
-
-
     </div>
 
 
@@ -22,49 +20,192 @@ class Calculator extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      parts: [],
+      prevNum: null,
+      currentNum: 0,
+      currentOp: null,
       currentDisplay: 0,
-      result: 0
+      clearDisplayNext: false
     }
   }
+
+  handleNumClick(num) {
+    let addDigitToDisplay;
+
+    if(this.state.clearDisplayNext){
+      addDigitToDisplay = num;
+    } else {
+      addDigitToDisplay = this.state.currentDisplay * 10 + num;
+    }
+
+    this.setState( (state) => {
+      return {
+        currentDisplay: addDigitToDisplay,
+        currentNum: addDigitToDisplay,
+        clearDisplayNext: false
+      };
+    });
+  }
+
+  handleOpClick(op) {
+    if(op === "=" && this.state.currentOp){
+      
+      if(this.state.currentOp === "+"){
+        this.setState( (state) => {
+          let result = this.state.prevNum + this.state.currentNum;
+          return {
+            currentDisplay: result,
+            currentNum: result,
+            currentOp: null,
+            prevNum: null
+          }
+        });
+      }
+
+      if(this.state.currentOp === "+"){
+        this.setState( (state) => {
+          let result = this.state.prevNum + this.state.currentNum;
+          return {
+            currentDisplay: result,
+            currentNum: result,
+            currentOp: null,
+            prevNum: null
+          }
+        });
+      }
+
+      if(this.state.currentOp === "-"){
+        this.setState( (state) => {
+          let result = this.state.prevNum - this.state.currentNum;
+          return {
+            currentDisplay: result,
+            currentNum: result,
+            currentOp: null,
+            prevNum: null
+          }
+        });
+      }
+
+      if(this.state.currentOp === "x"){
+        this.setState( (state) => {
+          let result = this.state.prevNum * this.state.currentNum;
+          return {
+            currentDisplay: result,
+            currentNum: result,
+            currentOp: null,
+            prevNum: null
+          }
+        });
+      }
+
+      if(this.state.currentOp === "/"){
+        this.setState( (state) => {
+          let result = this.state.prevNum / this.state.currentNum;
+          return {
+            currentDisplay: result,
+            currentNum: result,
+            currentOp: null,
+            prevNum: null
+          }
+        });
+      }
+
+
+
+    }
+
+    this.setState((state) => {
+      return {
+        currentOp: op,
+        prevNum: this.state.currentNum,
+        clearDisplayNext: true
+      };
+    })
+  }
+
+
 
   render(){
     return(
       <div>
-      <Display output={this.state.result}/>
-      <ButtonsRow chars={["AC","x^y","x^2","/"]} />
-      <ButtonsRow chars={["7","8","9","x"]} />
-      <ButtonsRow chars={["4","5","6","-"]} />
-      <ButtonsRow chars={["1","2","3","+"]} />
-      <ButtonsRow chars={["0",".","+/-","="]} />
+        <Display output = {this.state.currentDisplay}/>
+
+        <div className="row">
+          <BtnForNum buttonNum={"AC"} onClick={() => this.handleNumClick(0)}/>
+          <BtnForNum buttonNum={"X^Y"} onClick={() => this.handleNumClick(0)}/>
+          <BtnForNum buttonNum={"X^2"} onClick={() => this.handleNumClick(0)}/>
+          <BtnForOperation operation={"/"} className="btn" onClick={() => this.handleOpClick("/")} />
+        </div>
+        <div className="row">
+          <BtnForNum buttonNum={7} onClick={() => this.handleNumClick(7)}/>
+          <BtnForNum buttonNum={8} onClick={() => this.handleNumClick(8)}/>
+          <BtnForNum buttonNum={9} onClick={() => this.handleNumClick(9)}/>
+          <BtnForOperation operation={"x"} className="btn" onClick={() => this.handleOpClick("x")} />
+        </div>
+        <div className="row">
+          <BtnForNum buttonNum={4} onClick={() => this.handleNumClick(4)}/>
+          <BtnForNum buttonNum={5} onClick={() => this.handleNumClick(5)}/>
+          <BtnForNum buttonNum={6} onClick={() => this.handleNumClick(6)}/>
+          <BtnForOperation operation={"-"} className="btn" onClick={() => this.handleOpClick("-")} />
+        </div>
+        <div className="row">
+          <BtnForNum buttonNum={1} onClick={() => this.handleNumClick(1)}/>
+          <BtnForNum buttonNum={2} onClick={() => this.handleNumClick(2)}/>
+          <BtnForNum buttonNum={3} onClick={() => this.handleNumClick(3)}/>
+          <BtnForOperation operation={"+"} className="btn" onClick={() => this.handleOpClick("+")} />
+        </div>
+        <div className="row">
+          <BtnForNum buttonNum={0} onClick={() => this.handleNumClick(0)}/>
+          <BtnForNum buttonNum={"."} onClick={() => this.handleNumClick(0)}/>
+          <BtnForNum buttonNum={0} onClick={() => this.handleNumClick(0)}/>
+          <BtnForOperation operation={"="} className="btn" onClick={() => this.handleOpClick("=")} />
+        </div>
+
       </div>
     );
   }
 }
 
+function BtnForNum(props){
+    let buttonNum = props.buttonNum;
+    return <button 
+      className="btn"
+      key={buttonNum}
+      onClick= {() => props.onClick()}
+      >
+      {buttonNum}
+    </button>;
+  }
+
+function BtnForOperation(props){
+  let btnOper = props.operation;
+  return <button 
+    className="btn"
+    key={btnOper}
+    onClick= {() => props.onClick()}
+    >
+    {btnOper}
+  </button>;
+}
+
+/* <ButtonsRow chars={["AC","x^y","x^2","/"]} />
+<ButtonsRow chars={["7","8","9","x"]} />
+<ButtonsRow chars={["4","5","6","-"]} />
+<ButtonsRow chars={["1","2","3","+"]} />
+<ButtonsRow chars={["0",".","+/-","="]} /> */
+
+// function Display(props){
+//   return (
+//       <div className="result row">{props.output}</div>
+//   );
+  
+// }
+
 function Display(props){
-  let value = props.output;
-  return <div className="result row">{value}</div>;
+  return (
+      <div className="result row">{props.output}</div>
+  );
+  
 }
-
-function ButtonsRow(props){
-  let items = props.chars;
-
-  let buttons = items.map(
-    (button) => {
-      return <button key={button} className="btn" onClick={
-        () => processBtnPress(button)
-        }>{button}</button>;
-    }
-  )
-  return <div className="row">{buttons}</div>;
-}
-
-function processBtnPress(char) {
-  console.log("pressed", char);
-}
-
-
 
 
 export default App;
